eed963975a094d66e9602dc0e6d814a30b207eba
bup <50534745+thedogbup@users.noreply.github.com>
2020-05-28 14:59:00 -0400

    Create template.m
    Added template file

---
+++ template.m
@@ -0,0 +1,7 @@
+clear, clc, format compact, close all
+%==[Flowerbox]=================
+%| Filename: template.m       |
+%| Purpose: v3                |
+%| bup                        |
+%| 5/28/20                    |
+%|____________________________|
