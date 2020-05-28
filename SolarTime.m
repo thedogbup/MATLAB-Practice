c53706442a652675d462fa4aeb1c20b9038758df
bup <50534745+thedogbup@users.noreply.github.com>
2020-05-28 15:03:42 -0400

    Create SolarTime.m
    Date/Time Excersise

---
+++ SolarTime.m
@@ -0,0 +1,7 @@
+clear, clc, format compact, close all
+%==[Flowerbox]=================
+%| Filename: SolarTime.m      |___
+%| Purpose: Calculates solar time |
+%| bup                        ____|
+%| 5/28/20                    |
+%|____________________________|
