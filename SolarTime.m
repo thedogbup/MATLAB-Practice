56fd68802bcf73b5760bb4f2464204d0a93868d0
bup <50534745+thedogbup@users.noreply.github.com>
2020-06-01 15:49:56 -0400

    SolarTime Update 3
    Added date prompt, still doesn't work yet

--- SolarTime.m
+++ SolarTime.m
@@ -34,12 +34,20 @@ if(abs(fix(utcO) - utcO) * 60 < 10)
 end
 tzone = tzone + abs(fix(utcO) - utcO) * 60;
 
-sel = input("Type 0 for the time now or anything else to enter a custom time: ");
+sel = input("Type 0 for the displayed time now or anything else to enter a custom time: ");
+
+now = datetime("now");
 
 if(sel == 0)
-    now = datetime("now");    
-    
     timeIn = datetime(now.Year,now.Month,now.Day,now.Hour,now.Minute,now.Second,'TimeZone',tzone,...
     'Format','dd-MMM-yyyy HH:mm:ss Z');
+else
+    dateIn = input("Type 0 for the displayed date now or enter a date (dd-MMM-yyyy) at or after 1972: ");
+    while(str2double(substr(dateIn, 7, 11)) < 1972)
+        dateIn = input("Please enter a vaid date (dd-MMM-yyyy) with a valid year at or after 1972: ");
+    end
+    if(dateIn == 0)
+        dateIn = datetime("today");
+    end
 end
 
