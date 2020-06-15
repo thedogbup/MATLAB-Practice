e3800acecdde12329fff5208cbf517a64b329127
bup <50534745+thedogbup@users.noreply.github.com>
2020-06-14 22:48:22 -0400

    SolarTime Update 4
    Removed error checking, there should be better ways to do it with regex and error handling, but since this is for educational use, I figure removing them will help me finish this sooner

--- SolarTime.m
+++ SolarTime.m
@@ -9,15 +9,8 @@ clear, clc, format compact, close all
 disp("This app allows you to display the solar time for a given location.");
 
 longitude = input("Enter your chosen longitude in a range of [-180, 180]: ");
-while(longitude < -180 || longitude > 180)
-    longitude = input("Please enter a valid longitude in a range of [-180, 180]: ");
-end
 
 utcO = input("Type the specified UTC offset as a decimal from -12:00 to +14:00 (adding 0.5 for a 30 minute offset): ");
-
-while(utcO < -12 || utcO > 14)
-    utcO = input("Please enter a valid UTC offset as a decimal from -12:00 to +14:00 (adding 0.5 for a 30 minute offset): ");
-end
 
 if(utcO < 0)
         tzone = "-";
@@ -39,15 +32,25 @@ sel = input("Type 0 for the displayed time now or anything else to enter a custo
 now = datetime("now");
 
 if(sel == 0)
-    timeIn = datetime(now.Year,now.Month,now.Day,now.Hour,now.Minute,now.Second,'TimeZone',tzone,...
+    allIn = datetime(now.Year,now.Month,now.Day,now.Hour,now.Minute,now.Second,'TimeZone',tzone,...
     'Format','dd-MMM-yyyy HH:mm:ss Z');
 else
-    dateIn = input("Type 0 for the displayed date now or enter a date (dd-MMM-yyyy) at or after 1972: ");
-    while(str2double(substr(dateIn, 7, 11)) < 1972)
-        dateIn = input("Please enter a vaid date (dd-MMM-yyyy) with a valid year at or after 1972: ");
+    timeIn = [now.Year,now.Month,now.Day,now.Hour,now.Minute,now.Second];
+    ymd = input('Type 0 for the displayed date now or enter a date (dd-MMM-yyyy) at or after 1972: ');
+    if(ymd ~= 0)
+        timeIn(0) = ymd(8:12);
+        timeIn(1) = ymd(4:7);
+        timeIn(2) = ymd(1:2);
     end
-    if(dateIn == 0)
-        dateIn = datetime("today");
+    hms = input('Type 0 for the displayed time now or enter a time (HH:mm:ss): ');
+    if(hms ~= 0)
+        timeIn(3) = hms(1:2);
+        timeIn(4) = hms(4:5);
+        timeIn(5) = hms(7:8);
     end
+    
+    allIn = datetime(timeIn);
 end
+
+
 
