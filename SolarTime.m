7a21d876dd2b12e1d83e21ed1e52f9d39e9a0f90
bup <50534745+thedogbup@users.noreply.github.com>
2020-06-16 12:17:59 -0400

    SolarTime Update 6
    I'm convinced MATLAB hates me.

--- SolarTime.m
+++ SolarTime.m
@@ -18,12 +18,12 @@ if(utcO < 0)
         tzone = "+";
 end
 if(abs(utcO) < 10)
-    tzone = tzone + '0';
+    tzone = tzone + "0";
 end
-tzone = tzone + floor(abs(utcO)) + ':';
+tzone = tzone + floor(abs(utcO));
 
 if(abs(fix(utcO) - utcO) * 60 < 10)
-    tzone = tzone + '0';
+    tzone = tzone + "0";
 end
 tzone = tzone + abs(fix(utcO) - utcO) * 60;
 
@@ -37,19 +37,20 @@ if(sel == 0)
 else
     timeIn = [now.Year,now.Month,now.Day,now.Hour,now.Minute,now.Second];
     ymd = input('Type 0 for the displayed date now or enter a date (dd-MM-yyyy) at or after 1972: ', 's');
-    if(ymd ~= 0)
+    if(ymd ~= '0')
         timeIn(1) = str2double(ymd(7:10));
         timeIn(2) = str2double(ymd(4:5));
         timeIn(3) = str2double(ymd(1:2));
     end
     hms = input('Type 0 for the displayed time now or enter a time (HH:mm:ss): ', 's');
-    if(hms ~= 0)
+    if(hms ~= '0')
         timeIn(4) = str2double(hms(1:2));
         timeIn(5) = str2double(hms(4:5));
         timeIn(6) = str2double(hms(7:8));
     end
     
-    allIn = datetime(strcat(string(datetime(timeIn), " ", tzone)), 'InputFormat', 'dd-MM-yyyy HH:mm:ss ZZZZZ');
+    allIn = datetime(timeIn(1),timeIn(2),timeIn(3),timeIn(4),timeIn(5),timeIn(6),'TimeZone',tzone,...
+    'Format','dd-MM-yyyy HH:mm:ss Z');
 end
 
 
