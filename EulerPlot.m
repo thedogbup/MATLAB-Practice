b00a0d7bc3a16cbb363a9c1d9fa31ac43791844b
bup <50534745+thedogbup@users.noreply.github.com>
2020-06-29 21:16:38 -0400

    Update EulerPlot.m

--- EulerPlot.m
+++ EulerPlot.m
@@ -9,8 +9,11 @@ clear, clc, format compact, close all
 y = zeros(17);
 y(1) = 1;
 
-for i = 2:16,
+for i = 2:17
     y(i) = y(i - 1) + 1 / (16 * y(i - 1));
 end
-x = linspace(0, 1, 16);
-plot(x, y, 'go', x, sqrt(2 * x + 1))
+x = linspace(0, 1, 17);
+subplot(1, 2, 1);
+plot(x, y, '-g', x, sqrt(2 * x + 1), '-b')
+subplot(1, 2, 2)
+plot(x, abs(sqrt(2 * x + 1) - y), 'mx')
